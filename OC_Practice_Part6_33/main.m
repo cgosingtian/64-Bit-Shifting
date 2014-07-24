3e5ba0d5de7222bff279b8506a007d23fdb93741
Chase Gosingtian <c.gosingtian@klab.com>
2014-07-24 09:48:24 +0800

    Added a function to convert an integer to binary, represented by a string. There's also a method that returns an integer with alternating bits, but it's getting zero-ed out since the application is 32 bit, but we're trying to get 64 bits. I'm going to try using uint64 instead of NSUInteger.

--- OC_Practice_Part6_33/main.m
+++ OC_Practice_Part6_33/main.m
@@ -8,15 +8,55 @@
 
 #import <Foundation/Foundation.h>
 
+int const NUM_BITS = 64;
+
+NSString* bitString(NSUInteger num);
+NSUInteger generateAlternatingBitsOdd();
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        NSUInteger bit64 = 0;
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        bit64 = generateAlternatingBitsOdd();
+        
+        NSLog(@"%@, %lu", bitString(bit64), (unsigned long)bit64);
         
     }
     return 0;
 }
 
+NSUInteger generateAlternatingBitsOdd()
+{
+    NSUInteger num = 0;
+    NSUInteger exponent=0;
+    for (NSUInteger i = 0; i < NUM_BITS;i++)
+    {
+        exponent = (double)i*2.0f;
+        num += pow(2.0,exponent);
+        NSLog(@"%lu",(unsigned long)num);
+    }
+    return num;
+}
+
+//converts an unsigned int into binary as a string
+NSString* bitString(NSUInteger num)
+{
+    NSString *str = @"";
+    
+    for (NSUInteger i = 0;i < NUM_BITS;i++)
+    {
+        //prepend 1 to string if (num & 1) is 1, otherwise, 0
+        str = [NSString stringWithFormat:@"%@%@",
+               num & 1 ? @"1" : @"0", // ternary operator (aka if else)
+               str];
+        
+//        if (i % 8 == 0)
+//            str = [NSString stringWithFormat:@"%@ ",str];
+        
+        num >>= 1;
+    }
+    
+    return str;
+}
